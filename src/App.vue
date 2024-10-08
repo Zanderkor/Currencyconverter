681195c61d9269ae1bab0c3545a5a3d6b0b93215
ZanderKor <korund33@gmail.com>
2024-10-08 16:15:59 +0300

    FirstDay

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Валютный менеджер</h1>
+  <h2>Выберете валюту</h2>
+
+</template>
+
+<script >
+</script>
+
+<style scoped>
+h1 {
+  font-weight: bolder;
+  color: rgb(65, 138, 87);
+  position: top;
+
+
+}
+</style>
+<!--src="https://www.cbr-xml-daily.ru/money.js"-->
